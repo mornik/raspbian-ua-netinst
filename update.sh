b6c5157c12c1e56799a7a9e4bd07b0e2ae22069f
Alexandre LUCAZEAU <alexandre.lucazeau@free.fr>
2014-02-26 20:08:07 +0100

    update.sh : change sh to bash             Adding debug function and debug trace. Change value of DEBUG             to 1 to activate trace

--- update.sh
+++ update.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+export DEBUG=1
+export LOG=$(pwd)/log
+>$LOG
+debug(){
+if [ $DEBUG -eq 0 ]; then
+    echo "DEBUG : line $LINENO : $1"|tee -a $LOG
+fi
+}
 
 mirror=http://archive.raspbian.org/raspbian/
 release=jessie
@@ -30,38 +38,41 @@ allfound() {
 
 rm -rf packages/
 mkdir packages
+debug "clean directory"
 cd packages
 
 echo "Downloading package list..."
 wget -O - $mirror/dists/$release/firmware/binary-armhf/Packages.bz2 | bunzip2 -c > Packages
 wget -O - $mirror/dists/$release/main/binary-armhf/Packages.bz2 | bunzip2 -c >> Packages
+debug "downloading package list in packages directory"
 
 echo "Searching for required packages..."
 while read k v
 do
-        current_package=
-        current_filename=
+    current_filename=""
     if [ "$k" = "Package:" ]; then
         current_package=$v
+        debug "package : $v"
     fi
 
     if [ "$k" = "Filename:" ]; then
         current_filename=$v
+        debug "filename $v"
     fi
 
     if [ ! -z "$current_package" ] && [ ! -z "$current_filename" ]; then
+        debug "we have package and filename"
         if required $current_package; then
             printf "  %-32s %s\n" $current_package `basename $current_filename`
             packages_debs="${mirror}${current_filename} ${packages_debs}"
             packages_found="$current_package $packages_found"
-            echo $packages_found
+            debug $packages_found
             allfound && break
         fi
 
     fi
 done < Packages
 allfound || exit
-echo toto
-echo $packages_debs
+debug "list of package to download: $packages_debs"
 wget $packages_debs
 cd ..
